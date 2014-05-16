537bc044523a5665e82bd030580996a8407ec562
gruzovator <gruzovator@gmail.com>
2014-05-16 08:28:50 +0400

    more light bg + fork me on github banner

--- web/public/js/ctrl.js
+++ web/public/js/ctrl.js
@@ -174,7 +174,7 @@ function Init() {
         i.show();
     };
 
-    $('a').on('click', function(e){e.preventDefault();});
+    $('.content a').on('click', function(e){e.preventDefault();});
     $('a.tab_link').on('click', function(e){
         var title = e.target.getAttribute('title');
         $('#menu a').removeClass('active');
@@ -196,4 +196,7 @@ function Init() {
         ActivateTab('answer', qa_id);
     }
     else ActivateTab('ask');
+
+    // Fork me on github
+    $('body').append('<a href="https://github.com/gruzovator/AnsweringMachine"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>')
 }
